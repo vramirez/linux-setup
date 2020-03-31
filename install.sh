f8f9753216870e08cff66de560c81cb222d45d54
Victor M. Ramirez <ramirez.vmanuel@gmail.com>
2020-03-30 20:45:18 -0500

    Added docker to installer

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 CONDA_HOME=$HOME/miniconda
 
 sudo apt update -y
-sudo apt install gnome-tweak-tool git tlp tlp-rdw acpi-call-dkms tp-smapi-dkms acpi-call-dkms build-essential python3-dev libdbus-glib-1-dev libgirepository1.0-dev libcairo2-dev python3-venv python3-wheel  -y
+sudo apt install gnome-tweak-tool git tlp tlp-rdw acpi-call-dkms tp-smapi-dkms acpi-call-dkms build-essential python3-dev libdbus-glib-1-dev libgirepository1.0-dev libcairo2-dev python3-venv python3-wheel curl apt-transport-https ca-certificates software-properties-common -y 
 git clone https://github.com/erpalma/lenovo-throttling-fix.git
 sudo ./lenovo-throttling-fix/install.sh
 
 sudo mv /usr/share/budgie-extras-daemon/tilix_alt.bde  /usr/share/budgie-extras-daemon/tilix_alt.bde.bak
 sudo cp tlp.txt /etc/default/tlp
 sudo cp lenovo_fix.conf /etc/lenovo_fix.conf
+
+echo "Installing Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+
+
 echo "Downloading Miniconda and continuing..."
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh&
