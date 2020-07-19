cbfb0f4e2e4f6563640d124091ec74c4e0def102
Victor M. Ramirez <ramirez.vmanuel@gmail.com>
2020-07-19 10:52:46 -0500

    Added snap installer and some spacing

--- arch-installh.sh
+++ arch-installh.sh
@@ -1,11 +1,14 @@
-#Refresh outdated keyring
+# Refresh outdated keyring
 sudo pacman --no-confirm -Sy archlinux-keyring manjaro-keyring
 sudo pacman-key --populate archlinux manjaro
 sudo pacman-key --refresh-keys
 sudo pacman --no-confirm -Syu
 
-#Install TLP
+# Tilix terminal
 pacman --no-confirm -Sy tilix
+
+
+# Install TLP
 pacman --no-confirm -S tlp tlp-rdw acpi_call tp_smapi
 systemctl enable tlp.service
 systemctl enable NetworkManager-dispatcher.service
@@ -17,11 +20,22 @@ systemctl mask systemd-rfkill.socket
 pacman --no-confirm -S throttled
 sudo systemctl enable --now lenovo_fix.service
 
-#Install docker
+# Install docker
 sudo pacman --no-confirm -S docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
-# Common installation (Snap, configs, etc)
+# Setup snap
+
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+
+#  Common installation (Snap, configs, etc)
 sh common/all-common.sh
+
+
