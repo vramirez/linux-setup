6aa91cfc81c8990e2a13395d0c34b9f8aea3b952
Victor M. Ramirez <ramirez.vmanuel@gmail.com>
2020-07-19 10:26:02 -0500

    Added refresh keyring and no prompt to pacman

--- arch-installh.sh
+++ arch-installh.sh
@@ -1,7 +1,12 @@
-sudo pacman -Syu
-#Install TLP
+#Refresh outdated keyring
+sudo pacman --no-confirm -Sy archlinux-keyring manjaro-keyring
+sudo pacman-key --populate archlinux manjaro
+sudo pacman-key --refresh-keys
+sudo pacman --no-confirm -Syu
 
-pacman -S tlp tlp-rdw acpi_call tp_smapi
+#Install TLP
+pacman --no-confirm -Sy tilix
+pacman --no-confirm -S tlp tlp-rdw acpi_call tp_smapi
 systemctl enable tlp.service
 systemctl enable NetworkManager-dispatcher.service
 systemctl mask systemd-rfkill.service
@@ -9,11 +14,11 @@ systemctl mask systemd-rfkill.socket
 
 # Lenovo Throttling
 
-pacman -S throttled
+pacman --no-confirm -S throttled
 sudo systemctl enable --now lenovo_fix.service
 
 #Install docker
-sudo pacman -S docker
+sudo pacman --no-confirm -S docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
